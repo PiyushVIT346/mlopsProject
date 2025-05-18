e7e24431bf3f4b2704df3a4c9fb0b649b75ec3b4
piyush <piyushsingh50105@gmail.com>
2025-05-18 14:52:23 +0530

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,9 @@ import logging
 import os
 from datetime import datetime
 
-# Fix: use '=' instead of '-'
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs")
 
-# Create the 'logs' directory if it doesn't exist
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
